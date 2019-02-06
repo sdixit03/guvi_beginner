536b023367aa81af9d04c023b9e7c0be4aee9377
sdixit03 <40880273+sdixit03@users.noreply.github.com>
2019-02-06 15:58:41 +0530

    Update even_range.py

--- even_range.py
+++ even_range.py
@@ -1,8 +1,13 @@
 n=int(input())
 m=int(input())
-for i in range(n+1,m-1):
+for i in range(n+1,m):
     if(i%2==0):
         print(i)
+        
+        
+        
+        
+        
         
         
         
